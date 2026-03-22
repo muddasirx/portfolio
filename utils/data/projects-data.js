94a6a080b37b7eca6532c75a2b7739d3883bbe34
Muddasir <muhammadmuddasir374@gmail.com>
2026-03-22 18:30:56 +0500

    project added

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -10,18 +10,18 @@ export const projectsData = [
     },
     {
         id: 2,
-        name: 'Django Notes App Deployment',
-        description: "Deployed a containerized Django application using Docker and Docker Compose with MySQL and Nginx in a multi-container architecture. Configured inter-container networking and reverse proxy handling, and hosted the complete stack on AWS EC2 to demonstrate real-world deployment and service orchestration.",
-        tools: ['Docker', 'Docker Compose', 'MySQL', 'AWS EC2', 'Nginx', 'Linux'],
-        role: 'DevOps Engineer',
-        code: '',
-        demo: '',
-    },
-    {
-        id: 3,
         name: 'Node.js Todo App Deployment with CI/CD',
         description: "Built and deployed a Node.js Todo application using Docker and Docker Compose, integrated with a GitLab CI/CD pipeline. Automated build, test, image push to Docker Hub, and deployment to AWS EC2, demonstrating a complete continuous integration and delivery workflow.",
         tools: ['Node.js', 'Docker' , 'Docker Compose', 'GitLab CI/CD', 'AWS EC2', 'Linux'],
+        code: '',
+        role: 'DevOps Engineer',
+        demo: '',
+    }
+    {
+        id: 3,
+        name: 'Flask App Deployment on AWS ECS',
+        description: "Containerized a Flask application using Docker and pushed it to AWS ECR for version management. Deployed it on an ECS cluster with task definitions and auto-scaling to handle load efficiently, while using CloudWatch for monitoring. This project demonstrates a transition from manual EC2 deployment to a scalable, managed ECS-based architecture.",
+        tools: ['Docker' , 'AWS ECS' , 'ECR' , 'CloudWatch' , 'IAM' , 'EC2', 'Linux', 'Git'],
         code: '',
         role: 'DevOps Engineer',
         demo: '',
