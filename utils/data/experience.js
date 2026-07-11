2f9c71875817671198396db852ad545433d7a23c
Muhammad Muddasir <150518475+muddasirx@users.noreply.github.com>
2026-07-12 03:15:28 +0500

    Reorder experiences and update DevOps Engineer entry

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,15 @@
 export const experiences = [
   {
     id: 1,
-    title: 'DevOps Engineer',
-    company: "FreeLancer (Remote)",
-    duration: "(Jan 2026 - April 2026)"
-  },
-  {
-    id: 2,
     title: "Cloud Intern",
     company: "Cloudelligent (Onsite)",
     duration: "(May 2026 - Present)"
+  },
+  {
+    id: 2,
+    title: 'DevOps Engineer',
+    company: "FreeLancer (Remote)",
+    duration: "(Jan 2026 - April 2026)"
   },
   // {
   //   id: 3,
